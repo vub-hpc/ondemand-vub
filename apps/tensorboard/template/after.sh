c62af9b4211b90cb23930fd4ecc039aaae163f71
Alex Domingo <alex.domingo.toro@vub.be>
2025-07-10 11:36:40 +0200

    use common template to wait for server response in after script

--- apps/tensorboard/template/after.sh
+++ apps/tensorboard/template/after.sh
@@ -1,18 +1,6 @@
-# wait for the Tensorboard server to start
-echo "$(date): waiting for TensorBoard server to open port ${port}..."
 time1=$(date +%s)
 
-if wait_until_port_used "${host}:${port}" ${MAXWAIT:-180}; then
-    echo "$(date): discovered TensorBoard server listening on port ${port}!"
-
-else
-    echo "$(date): timed out waiting for TensorBoard server to open port ${port}!"
-    echo "- wait ended at: $(date)"
-    pkill -P ${SCRIPT_PID}
-    clean_up 1
-fi
-
-sleep 2
+<%= ERB.new(File.read('../common_files/wait_server_start.sh.erb'), eoutvar: 'child').result(binding) %>
 
 function wait_until_tb_ready () {
     logfile=$1
@@ -29,7 +17,6 @@ time2=$(date +%s)
 
 if wait_until_tb_ready "$OOD_SESSION_STAGED_ROOT/tensorboard.log" $(( ${MAXWAIT:-180} - time2 + time1 )); then
     echo "$(date): TensorBoard server ready!"
-
 else
     echo "$(date): timed out waiting for TensorBoard server to get ready!"
     echo "- wait ended at: $(date)"
