411d16ca84793803b4505993cd8b55fce307a672
Samuel Moors <sammoors@gmail.com>
2026-02-11 15:35:28 +0100

    add comment

--- files/html/global_form.js
+++ files/html/global_form.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function resetGPUs() {
+  // set the number of GPUs to 0 when changing to the "automatic" partition
   const $queue = $('#batch_connect_session_context_auto_queues');
   const $gpus  = $('#batch_connect_session_context_dynamic_num_gpu_slots');
 
