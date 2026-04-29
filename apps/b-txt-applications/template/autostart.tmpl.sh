9ef44de813bc681aea133d947deb466694cebb93
yoshi <yoshimalaise@hotmail.com>
2026-04-29 07:45:58 +0200

    process feedback on PR

--- apps/b-txt-applications/template/autostart.tmpl.sh
+++ apps/b-txt-applications/template/autostart.tmpl.sh
@@ -1,16 +1,22 @@
 mkdir -p $B_TXT_DATA
 
-# Optionally use private namespace (maybe with slirp4netns)
-# unshare -rn bash -eu <<EOF
-# ip link set lo up
+# get free port - taken from https://stackoverflow.com/a/78125240
+local_app_port=15000
+
+while [ -n "$(ss -tan4H "sport = $local_app_port")" ]; do
+  local_app_port=$((port+1))
+done
+
+echo "Usable Port: $local_app_port"
 
 # APP_IMAGE is the path to image (should be in /apps/brussel/containers)
 case "$APP_CHOICE" in
   label-studio )
     APP_IMAGE="/apps/brussel/containers/label-studio/label-studio.sif"
     apptainer run \
-      --env "WEBSERVER_PORT=$WEBSERVER_PORT" \
+      --env "WEBSERVER_PORT=${local_app_port}" \
       --bind $B_TXT_DATA:/app/data \
+      --contain \
       $APP_IMAGE &>> "$OOD_SESSION_STAGED_ROOT/label-studio.log" &
     app_pid=\$!
     URL_SUFFIX=""
@@ -23,15 +29,13 @@ case "$APP_CHOICE" in
 esac
 
 echo "Waiting for webserver to start..."
-until curl --output /dev/null --silent --head --fail http://localhost:$WEBSERVER_PORT; do
+until curl --output /dev/null --silent --head --fail http://localhost:${local_app_port}; do
     sleep 1
 done
 echo "$APP_CHOICE webserver is up."
 
 # Create new firefox profile and start new isolated instance with that profile
 firefox -CreateProfile "$SLURM_JOB_ID /tmp/ff-profile-$SLURM_JOB_ID"
-firefox -no-remote -P "$SLURM_JOB_ID" localhost:$WEBSERVER_PORT$URL_SUFFIX
+firefox -no-remote -P "$SLURM_JOB_ID" localhost:${local_app_port}$URL_SUFFIX
 
 [[ -n \$app_pid ]] && env kill --verbose \$app_pid
-
-#EOF 
